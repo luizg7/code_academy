470bc2dba37d4db73f45a12fa3d1c0feff4d05a2
BoBgit config --global user.name BoBgit config --global user.email <luizg7@hotmail.com>
2018-08-31 16:41:04 -0300

    Added "Control Flow" studies.

---
+++ 02_else_statements.js
@@ -0,0 +1,5 @@
+let sale = true;
+
+if (sale) {
+  console.log('Time to buy!')
+}
---
+++ 10_the_switch_keyword.js
@@ -0,0 +1,32 @@
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+  break;
+  case 'second place':
+    console.log('You get the silver medal!');
+  break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+  break;
+  default:
+    console.log('No medal awarded.');
+  break;
+}
+
+
+/* Syntax for th switch statement
+switch (condition) {
+  case firstCase :
+    code block;
+  break;
+  case secondCase :
+    code block;
+  break;
+  default :
+  code block;
+  break;
+}
+
+*/
