dbabdd7ec61b8faeabdff277efa82a2a860f1a3a
BoBgit config --global user.name BoBgit config --global user.email <luizg7@hotmail.com>
2018-09-05 12:56:40 -0300

    Added Scope

---
+++ scope.03-global_and_scope.js
@@ -0,0 +1,11 @@
+let satellite = 'The Moon';
+
+let galaxy = 'The Milky Way';
+
+let stars = 'North Star';
+
+function callMyNightSky () {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+}
+
+console.log(callMyNightSky())
